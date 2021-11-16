60279f5d3ff7e03fda6ae3ca59b136678c4cffe9
Milano Hoogvliets <milanohoogvliets@hotmail.com>
2021-11-17 00:42:42 +0100

    MainActivity and HomeActivity added

---
+++ app/src/main/java/com/example/bike_solution/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.bike_solution;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.home);
+
+    }
+}
--- app/src/main/java/com/example/bike_solution/MainActivity.java
+++ app/src/main/java/com/example/bike_solution/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.bike_solution;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.login);
+
+        login = findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
